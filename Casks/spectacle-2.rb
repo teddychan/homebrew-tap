582af2ac5fff39c4977fbedbf2b3109ed678991e
Teddy Chan <teddychan@users.noreply.github.com>
2026-07-07 21:34:54 +0900

    spectacle-2: add cask (v2.0.0, sha filled by release CI)

---
+++ Casks/spectacle-2.rb
@@ -0,0 +1,24 @@
+cask "spectacle-2" do
+  version "2.0.0"
+  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+
+  url "https://github.com/teddychan/spectacle-2/releases/download/v#{version}/Spectacle2-v#{version}.zip"
+  name "Spectacle 2"
+  desc "Move and resize windows with keyboard shortcuts"
+  homepage "https://github.com/teddychan/spectacle-2"
+
+  auto_updates true
+  depends_on macos: :tahoe
+
+  app "Spectacle 2.app"
+
+  uninstall quit: "com.dragonapp.spectacle-2"
+
+  zap trash: [
+    "~/Library/Application Support/com.dragonapp.spectacle-2",
+    "~/Library/Caches/com.dragonapp.spectacle-2",
+    "~/Library/HTTPStorages/com.dragonapp.spectacle-2",
+    "~/Library/Preferences/com.dragonapp.spectacle-2.plist",
+    "~/Library/Saved Application State/com.dragonapp.spectacle-2.savedState",
+  ]
+end
